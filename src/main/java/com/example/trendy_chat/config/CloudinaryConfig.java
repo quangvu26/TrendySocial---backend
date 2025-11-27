445391de9f867bf072e0b302be94a56957f2b92b
quangvu26 <vuquanghanoi2608@gmail.com>
2025-11-27 14:27:40 +0700

    new version cloudinary

--- src/main/java/com/example/trendy_chat/config/CloudinaryConfig.java
+++ src/main/java/com/example/trendy_chat/config/CloudinaryConfig.java
@@ -1,15 +1,18 @@
 package com.example.trendy_chat.config;
 
 import com.cloudinary.Cloudinary;
-import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CloudinaryConfig {
+
+    @Value("${CLOUDINARY_URL}")
+    private String cloudinaryUrl;
+
     @Bean
     public Cloudinary cloudinary() {
-        Dotenv dotenv = Dotenv.load();
-        return new Cloudinary(dotenv.get("CLOUDINARY_URL"));
+        return new Cloudinary(cloudinaryUrl);
     }
 }
